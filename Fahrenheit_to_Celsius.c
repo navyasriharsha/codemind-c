3b11670499e7a5f1d075519606bfdc53cf569297
navyasriharsha <84239608+navyasriharsha@users.noreply.github.com>
2022-10-06 02:04:21 +0530

    # include<stdio.h> int main() {     float radius, area;
        scanf("%f", & radius);  
        area = (radius-32)*5/9;
        printf("%0.2f
    ", area);  
        
        return 0;  
    }

---
+++ Fahrenheit_to_Celsius.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()  
+{  
+    float radius, area;  
+ 
+    scanf("%f", & radius);  
+    area = (radius-32)*5/9;
+    printf("%0.2f
+", area);  
+    
+    return 0;  
+}   
