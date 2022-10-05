7504194a10d9b3ea9a154648f75e669e96323107
navyasriharsha <84239608+navyasriharsha@users.noreply.github.com>
2022-10-06 02:29:11 +0530

    # include<stdio.h> int main() {     int radius, area;
        scanf("%d", & radius);  
        area = radius*(radius-3)/2;
        printf("%d", area);  
        
        return 0;  
    }

---
+++ Diagonals_of_polygon.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main()  
+{  
+    int radius, area;  
+ 
+    scanf("%d", & radius);  
+    area = radius*(radius-3)/2;
+    printf("%d", area);  
+    
+    return 0;  
+}   
