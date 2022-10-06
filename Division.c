23c1a6418a84bb19acfc47d0d82df6f94fd2a40e
navyasriharsha <84239608+navyasriharsha@users.noreply.github.com>
2022-10-06 13:02:34 +0530

    # include<stdio.h> int main() {     int radius1,radius2, area;
        scanf("%d", & radius1);  
        scanf("%d", & radius2);  
        area = radius1 /radius2;  
        printf("%d", area);  
        
        return 0;  
    }

---
+++ Division.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()  
+{  
+    int radius1,radius2, area;  
+ 
+    scanf("%d", & radius1);  
+    scanf("%d", & radius2);  
+    area = radius1 /radius2;  
+    printf("%d", area);  
+    
+    return 0;  
+}   
