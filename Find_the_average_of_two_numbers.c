776e5ee0a6fa04297e825267983882020179d80b
navyasriharsha <84239608+navyasriharsha@users.noreply.github.com>
2022-10-06 13:21:18 +0530

    #include<stdio.h> #include<math.h>
    int main()
    {
        float a, b, s;
        
        scanf("%f%f",&a,&b);
        s = (a+b)/2;
     
        //Calculate area of triangle
    
     
        //Area with 2 digits of precision
        printf("%.4f
    ",s);
     
        return 0;
    }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+ 
+int main()
+{
+    float a, b, s;
+    
+    scanf("%f%f",&a,&b);
+    s = (a+b)/2;
+ 
+    //Calculate area of triangle
+
+ 
+    //Area with 2 digits of precision
+    printf("%.4f
+",s);
+ 
+    return 0;
+}
