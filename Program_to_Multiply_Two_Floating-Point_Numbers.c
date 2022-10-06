6d64cb6cfd876d35ff09eec11177559e3ec172a3
navyasriharsha <84239608+navyasriharsha@users.noreply.github.com>
2022-10-06 13:15:49 +0530

    # include<stdio.h> int main() {     float radius1,radius2 ,area;
        scanf("%f", & radius1); 
          scanf("%f", & radius2);
        area =radius1 * radius2;  
        printf("%0.2f
    ", area);  
        
        return 0;  
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()  
+{  
+    float radius1,radius2 ,area;  
+ 
+    scanf("%f", & radius1); 
+      scanf("%f", & radius2);
+    area =radius1 * radius2;  
+    printf("%0.2f
+", area);  
+    
+    return 0;  
+}   
